6201d7f3f9ea4f63a698197e28522eb1763c35e4
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-14 01:45:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,6 +11,18 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+   
+@bot.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 724335733841854514 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(726315919093137458)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
 
 @bot.command()
