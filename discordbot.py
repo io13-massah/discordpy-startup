27e495d3051427bbacc2ff7ddce75f34d59527bf
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-14 01:59:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -37,6 +37,9 @@ async def zippo(ctx):
 async def じっぽ(ctx):
     await ctx.send('世界で1番可愛い20歳の女の子、じっぽちゃん☆趣味は麻雀、好きな食べ物は酒とタバコ！推してもいいんだからね♡(・ω＜)★')
 
+@bot.command()
+async def Zippo(ctx):
+    await ctx.send('世界で1番可愛い20歳の女の子、じっぽちゃん☆趣味は麻雀、好きな食べ物は酒とタバコ！推してもいいんだからね♡(・ω＜)★')
 
     
 bot.run(token)
