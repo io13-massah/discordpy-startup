4073875bc35eff40604855ac8e496d28b6ee8994
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-14 00:55:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,18 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+from datetime import datetime, timedelta
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
 
+@client.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 724335733841854514 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(726315919093137458)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run("zMyMDM5NzE0MTk4MjU3NzQ1.XwxWCg.8EORwMtGCPPh_BXarSLsp3sr_mg")
