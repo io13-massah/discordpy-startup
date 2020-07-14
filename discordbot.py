157d970d42e8da41bc8af2f277c84ca9d1e574c5
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-15 02:48:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import traceback
 
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
+bot = commands.Bot(command_prefix='/')
 
 @client.event
 async def on_voice_state_update(member, before, after): 
@@ -13,5 +14,16 @@ async def on_voice_state_update(member, before, after):
         if before.channel is None: 
             msg = f'{member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
+            
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+@bot.command()
+async def moni(ctx):
+    await ctx.send('もにわぁ〜令和生まれのぴちぴちギャルだよぉ★')
 
 client.run(token)
+bot.run(token)
