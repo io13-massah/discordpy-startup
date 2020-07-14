fb6910f6b4bf809c99312250c411b83058ed8449
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-15 02:43:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,13 +9,9 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @client.event
 async def on_voice_state_update(member, before, after): 
     if member.guild.id == 724335733841854514 and (before.channel != after.channel):
-        now = datetime.utcnow() + timedelta(hours=9)
         alert_channel = client.get_channel(726315919093137458)
         if before.channel is None: 
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
-            await alert_channel.send(msg)
-        elif after.channel is None: 
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            msg = f'{member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
 
 client.run(token)
