db34a0a523b3ff71cc05354bf99fc1cec07857ec
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-14 20:51:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,32 @@
-import discord
-from datetime import datetime, timedelta
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-# 通知用チャンネルID
-inform_channnel = 726315919093137458
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-#通知
-@client.event
-async def on_voice_state_update(member, before, after):
+@bot.command()
+async def moni(ctx):
+    await ctx.send('もにわぁ〜令和生まれのぴちぴちギャルだよぉ★')
+
+@bot.command()
+async def zippo(ctx):
+    await ctx.send('世界で1番可愛い20歳の女の子、じっぽちゃん☆趣味は麻雀、好きな食べ物は酒とタバコ！推してもいいんだからね♡(・ω＜)★')
+
+@bot.command()
+async def じっぽ(ctx):
+    await ctx.send('世界で1番可愛い20歳の女の子、じっぽちゃん☆趣味は麻雀、好きな食べ物は酒とタバコ！推してもいいんだからね♡(・ω＜)★')
+
+@bot.command()
+async def Zippo(ctx):
+    await ctx.send('世界で1番可愛い20歳の女の子、じっぽちゃん☆趣味は麻雀、好きな食べ物は酒とタバコ！推してもいいんだからね♡(・ω＜)★')
+
     
-    if member.guild.id == 724335733841854514 and (before.channel != after.channel):
-        now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(726315919093137458)
-        if before.channel is None:
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
-            await alert_channel.send(msg)
-            
-        elif after.channel is None:
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から逃げました。'
-            await alert_channel.send(msg) 
-
-
-client.run(token)
+bot.run(token)
