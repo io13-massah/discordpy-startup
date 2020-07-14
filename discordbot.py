990837d6dfc891181585b00d47a15e141215a566
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-15 02:55:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ async def on_voice_state_update(member, before, after):
     if member.guild.id == 724335733841854514 and (before.channel != after.channel):
         alert_channel = client.get_channel(726315919093137458)
         if before.channel is None: 
-            msg = f'{member.name} が {after.channel.name} に参加しました。'
+            msg = f'@イオーナー {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
 
 @bot.command()
