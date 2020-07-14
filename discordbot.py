6b6c05b5d1cbe527beac77bfb268af2b01c44e1c
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-15 03:08:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,19 +5,20 @@ import traceback
 
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
-bot = commands.Bot(command_prefix='/')
 
 @client.event
 async def on_voice_state_update(member, before, after): 
     if member.guild.id == 724335733841854514 and (before.channel != after.channel):
         alert_channel = client.get_channel(726315919093137458)
         if before.channel is None: 
-            msg = f'{message.author.mention} {member.name} が {after.channel.name} に参加しました。'
+            msg = f'{member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
 
-@bot.command()
-async def moni(ctx):
-    await ctx.send('もにわぁ〜令和生まれのぴちぴちギャルだよぉ★')
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '/moni':
+        await message.channel.send('もにわぁ〜令和生まれのぴちぴちギャルだよぉ★')
 
 client.run(token)
-bot.run(token)
