96744a002d01b7f348a273e389962d42c16ec7d7
io13-massah <68232077+io13-massah@users.noreply.github.com>
2020-07-14 22:37:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,25 @@
 from discord.ext import commands
+from datetime import datetime, timedelta
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    
+    if member.guild.id == 724335733841854514 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(726315919093137458)
+        if before.channel is None:
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+            
+        elif after.channel is None:
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から逃げました。'
+            await alert_channel.send(msg) 
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -28,5 +43,16 @@ async def じっぽ(ctx):
 async def Zippo(ctx):
     await ctx.send('世界で1番可愛い20歳の女の子、じっぽちゃん☆趣味は麻雀、好きな食べ物は酒とタバコ！推してもいいんだからね♡(・ω＜)★')
 
-    
+@bot.command()
+async def usa(ctx):
+    await ctx.send('世界一魚臭い男、うさんくさいです。')
+
+@bot.command()
+async def うさ(ctx):
+    await ctx.send('世界一魚臭い男、うさんくさいです。')
+
+@bot.command()
+async def io(ctx):
+    await ctx.send('私は前に進む、生え際を置き去りにして。はじめまして、私が鯖主のイオです。')
+
 bot.run(token)
